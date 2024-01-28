5a886881855748e04a296cca4ce7c5cb700e0b09
Timothee Guerin <tiguerin@microsoft.com>
2024-01-27 18:46:43 -0800

    .

--- packages/github-pr-commenter/src/cli.ts
+++ packages/github-pr-commenter/src/cli.ts
@@ -5,26 +5,31 @@ try {
 } catch {
   // package only present in dev.
 }
-import { createActionAuth } from "@octokit/auth-action";
-import { Octokit } from "octokit";
+import { context, getOctokit } from "@actions/github";
 
 function log(...args: any[]) {
   // eslint-disable-next-line no-console
   console.log(...args);
 }
 
+const magicString = "<!--chronus-github-change-commenter-->";
 async function main() {
   const prNumber = process.env.GITHUB_EVENT_PULL_REQUEST_NUMBER;
   log("PR number", prNumber);
-  console.log("ENvs", process.env);
-  const github = new Octokit({ auth: createActionAuth() });
+  const token = process.env.GITHUB_TOKEN;
+  if (!token) {
+    throw new Error("GITHUB_TOKEN environment variable is not set");
+  }
+  const github = getOctokit(token);
 
-  // github.rest.issues.createComment({
-  //   issue_number: context.issue.number,
-  //   owner: context.repo.owner,
-  //   repo: context.repo.repo,
-  //   body: [].join("\n"),
-  // });
+  const content = [magicString, "Test comment"];
+
+  github.rest.issues.createComment({
+    issue_number: context.issue.number,
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    body: content.join("\n"),
+  });
 }
 
 await main();
