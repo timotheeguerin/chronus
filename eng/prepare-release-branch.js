d0b8f438dd049ff5f2925a6faa1e33713400453d
Timothee Guerin <tiguerin@microsoft.com>
2024-01-31 10:04:42 -0800

    Fix auto release (#36)

--- eng/prepare-release-branch.js
+++ eng/prepare-release-branch.js
@@ -1,8 +1,11 @@
+// @ts-check
+import { context, getOctokit } from "@actions/github";
 import { execSync } from "child_process";
-
+import { showStatusAsMarkdown } from "../packages/chronus/dist/cli/commands/show-status.js";
 const branchName = "publish/auto-release";
 
-execSync(`pnpm changeset version`);
+const changeStatus = await showStatusAsMarkdown(process.cwd());
+execSync(`pnpm change version`);
 const stdout = execSync(`git status --porcelain`).toString();
 
 if (stdout.trim() !== "") {
@@ -16,6 +19,31 @@ if (stdout.trim() !== "") {
   console.log("|  Link to create the PR");
   console.log(`|  https://github.com/timotheeguerin/chronus/pull/new/${branchName}  `);
   console.log("-".repeat(160));
+
+  const github = getOctokit(process.env.GITHUB_TOKEN ?? "");
+  const prs = await github.rest.pulls.list({
+    ...context.repo,
+    head: branchName,
+    base: "main",
+    state: "open",
+  });
+  const existing = prs.data[0];
+  if (existing) {
+    console.log("Existing, updating pr", existing.number);
+    await github.rest.pulls.update({
+      ...context.repo,
+      pull_number: existing.number,
+      body: changeStatus,
+    });
+  } else {
+    await github.rest.pulls.create({
+      ...context.repo,
+      title: "Release changes",
+      head: branchName,
+      base: "main",
+      body: changeStatus,
+    });
+  }
 } else {
   console.log("No changes to publish");
 }
--- packages/chronus/src/cli/commands/show-status.ts
+++ packages/chronus/src/cli/commands/show-status.ts
@@ -3,7 +3,7 @@ import type { VersionType } from "@changesets/types";
 import pc from "picocolors";
 import { resolveConfig } from "../../config/parse.js";
 import { assembleReleasePlan } from "../../release-plan/assemble-release-plan.js";
-import type { ReleaseAction } from "../../release-plan/types.js";
+import type { ReleaseAction, ReleasePlan } from "../../release-plan/types.js";
 import { NodechronusHost } from "../../utils/node-host.js";
 import { loadWorkspace } from "../../workspace-manager/auto-discover.js";
 
@@ -12,21 +12,7 @@ function log(...args: any[]) {
   console.log(...args);
 }
 export async function showStatus(cwd: string): Promise<void> {
-  const host = NodechronusHost;
-  const config = await resolveConfig(host, cwd);
-  const workspace = await loadWorkspace(host, config.workspaceRoot, config.workspaceType);
-
-  const changelogs = await host.glob(".changeset/*.md", { baseDir: workspace.path });
-  const changesets = await Promise.all(
-    changelogs
-      .filter((x) => x.toLowerCase() !== ".changeset/readme.md")
-      .map(async (x) => {
-        const file = await host.readFile(x);
-        return { ...parseChangeset(file.content), id: x.slice(".changeset/".length, -3) };
-      }),
-  );
-
-  const releasePlan = assembleReleasePlan(changesets, workspace, config);
+  const releasePlan = await resolveCurrentReleasePlan(cwd);
 
   log("");
   log(pc.bold("Change summary:"));
@@ -51,5 +37,52 @@ function logType(actions: ReleaseAction[], type: VersionType, color: (x: string)
           pc.cyan(action.newVersion),
       );
     }
+  }
+}
+
+async function resolveCurrentReleasePlan(cwd: string): Promise<ReleasePlan> {
+  const host = NodechronusHost;
+  const config = await resolveConfig(host, cwd);
+  const workspace = await loadWorkspace(host, config.workspaceRoot, config.workspaceType);
+
+  const changelogs = await host.glob(".changeset/*.md", { baseDir: workspace.path });
+  const changesets = await Promise.all(
+    changelogs
+      .filter((x) => x.toLowerCase() !== ".changeset/readme.md")
+      .map(async (x) => {
+        const file = await host.readFile(x);
+        return { ...parseChangeset(file.content), id: x.slice(".changeset/".length, -3) };
+      }),
+  );
+
+  const releasePlan = assembleReleasePlan(changesets, workspace, config);
+  return releasePlan;
+}
+
+export async function showStatusAsMarkdown(cwd: string): Promise<string> {
+  const releasePlan = await resolveCurrentReleasePlan(cwd);
+  return [
+    "",
+    "## Change summary:",
+    "",
+    ...typeAsMarkdown(releasePlan.actions, "major"),
+    "",
+    ...typeAsMarkdown(releasePlan.actions, "minor"),
+    "",
+    ...typeAsMarkdown(releasePlan.actions, "patch"),
+    "",
+  ].join("\n");
+}
+
+function typeAsMarkdown(actions: ReleaseAction[], type: VersionType): string[] {
+  const bold = (x: string) => `**${x}**`;
+  const filteredActions = actions.filter((x) => x.type === type);
+  if (filteredActions.length === 0) {
+    return [`### ${"No"} packages to be bumped at ${bold(type)}`];
+  } else {
+    return [
+      `### ${filteredActions.length} packages to be bumped at ${bold(type)}:`,
+      ...filteredActions.map((action) => `- ${action.packageName} \`${action.oldVersion}\` → \`${action.newVersion}\``),
+    ];
   }
 }
