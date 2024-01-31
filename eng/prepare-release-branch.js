5e9cc38ecd6e25e7fe018a83beb5b6ca25837987
Timothee Guerin <tiguerin@microsoft.com>
2024-01-31 09:51:01 -0800

    test

--- eng/prepare-release-branch.js
+++ eng/prepare-release-branch.js
@@ -1,10 +1,10 @@
 // @ts-check
 import { getOctokit } from "@actions/github";
 import { execSync } from "child_process";
-
+import { showStatusAsMarkdown } from "../packages/chronus/dist/cli/commands/show-status.js";
 const branchName = "publish/auto-release";
 
-const changeStatus = execSync(`pnpm change status`).toString();
+const changeStatus = await showStatusAsMarkdown(process.cwd());
 execSync(`pnpm change version`);
 const stdout = execSync(`git status --porcelain`).toString();
 
@@ -21,15 +21,33 @@ if (stdout.trim() !== "") {
   console.log("-".repeat(160));
 
   const github = getOctokit(process.env.GITHUB_TOKEN ?? "");
-  await github.rest.pulls.create({
-    // cspell:ignore timotheeguerin
+  const prs = github.rest.pulls.list({
     owner: "timotheeguerin",
     repo: "chronus",
-    title: "Release changes",
     head: branchName,
     base: "main",
-    body: changeStatus,
+    state: "open",
   });
+  const existing = prs[0];
+  if (existing) {
+    await github.rest.pulls.update({
+      // cspell:ignore timotheeguerin
+      owner: "timotheeguerin",
+      repo: "chronus",
+      pull_number: existing.number,
+      body: changeStatus,
+    });
+  } else {
+    await github.rest.pulls.create({
+      // cspell:ignore timotheeguerin
+      owner: "timotheeguerin",
+      repo: "chronus",
+      title: "Release changes",
+      head: branchName,
+      base: "main",
+      body: changeStatus,
+    });
+  }
 } else {
   console.log("No changes to publish");
 }
