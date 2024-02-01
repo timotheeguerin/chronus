716252c544880e659639bd36274c809604888f76
Timothee Guerin <tiguerin@microsoft.com>
2024-02-01 15:25:04 -0800

    fix tests

--- packages/chronus/src/workspace/load.ts
+++ packages/chronus/src/workspace/load.ts
@@ -3,7 +3,7 @@ import { resolveConfig } from "../config/parse.js";
 import type { ChronusResolvedConfig } from "../config/types.js";
 import type { ChronusHost } from "../utils/host.js";
 import { loadWorkspace } from "../workspace-manager/auto-discover.js";
-import type { Package } from "../workspace-manager/types.js";
+import type { Package, Workspace } from "../workspace-manager/types.js";
 import type { ChronusWorkspace } from "./types.js";
 
 function isPackageIgnored(config: ChronusResolvedConfig, pkg: Package) {
@@ -13,12 +13,15 @@ function isPackageIgnored(config: ChronusResolvedConfig, pkg: Package) {
 export async function loadChronusWorkspace(host: ChronusHost, dir: string): Promise<ChronusWorkspace> {
   const config = await resolveConfig(host, dir);
   const workspace = await loadWorkspace(host, config.workspaceRoot, config.workspaceType);
+  return createChronusWorkspace(workspace, config);
+}
 
+export function createChronusWorkspace(workspace: Workspace, config: ChronusResolvedConfig): ChronusWorkspace {
   return {
     path: config.workspaceRoot,
     workspace,
-    config,
-    packages: workspace.packages.filter((x) => !isPackageIgnored(config, x)),
+    packages: workspace.packages.filter((pkg) => !isPackageIgnored(config, pkg)),
     allPackages: workspace.packages,
+    config,
   };
 }
