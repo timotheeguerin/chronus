e9a2c1380e7f4590020e8725789a266e8128726c
Timothee Guerin <tiguerin@microsoft.com>
2024-03-01 21:08:31 -0800

    Get the data

--- packages/github/src/changelog/index.ts
+++ packages/github/src/changelog/index.ts
@@ -1,7 +1,9 @@
 import { NodeChronusHost, loadChronusWorkspace, type ChronusWorkspace } from "@chronus/chronus";
 import { readChangeDescriptions, resolveChangeRelativePath } from "@chronus/chronus/change";
 import { createGitSourceControl } from "@chronus/chronus/source-control/git";
+import { ChronusError, execAsync } from "@chronus/chronus/utils";
 import { resolve } from "path";
+import { getGithubInfoForChange } from "./fetch-pr-info.js";
 
 async function getCommitsThatAddChangeDescriptions(
   workspace: ChronusWorkspace,
@@ -11,15 +13,37 @@ async function getCommitsThatAddChangeDescriptions(
   const paths = changeDescriptionsFilename.map((id) => resolveChangeRelativePath(id));
   const commits = await git.getCommitsThatAddFiles(paths);
 
-  return commits;
+  const result: Record<string, string> = {};
+  for (const changeId of changeDescriptionsFilename) {
+    const commit = commits[resolveChangeRelativePath(changeId)];
+    if (commit) {
+      result[changeId] = commit;
+    }
+  }
+  return result;
 }
 
 const workspace = await loadChronusWorkspace(NodeChronusHost, resolve(process.cwd(), "../.."));
 
 const changes = await readChangeDescriptions(NodeChronusHost, workspace);
-console.log(
-  await getCommitsThatAddChangeDescriptions(
-    workspace,
-    changes.map((c) => c.id),
-  ),
+const commits = await getCommitsThatAddChangeDescriptions(
+  workspace,
+  changes.map((c) => c.id),
 );
+
+const result = await getGithubInfoForChange("timotheeguerin", "chronus", commits, await getGithubToken());
+
+console.log("Res", result);
+
+async function getGithubToken(): Promise<string> {
+  if (process.env.GITHUB_TOKEN) {
+    return process.env.GITHUB_TOKEN;
+  } else {
+    const result = await execAsync("gh", ["auth", "token"]);
+    if (result.code === 0) {
+      return result.stdout.toString().trim();
+    } else {
+      throw new ChronusError(`Failed to get github token:${result.stdall}`);
+    }
+  }
+}
