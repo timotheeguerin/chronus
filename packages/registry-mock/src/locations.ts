e0fc6f6c3142a02536f576e5bfbacbcd4e8c6d86
Timothee Guerin <tiguerin@microsoft.com>
2024-03-08 20:25:06 -0800

    cha

--- packages/registry-mock/src/locations.ts
+++ packages/registry-mock/src/locations.ts
@@ -4,7 +4,7 @@ import { join } from "path";
 const REGISTRY_MOCK_PORT = process.env["CHRONUS_REGISTRY_MOCK_PORT"] || "4873";
 
 const registry = () => join(__dirname, "../registry");
-const configPath = () => join(registry(), `runtime-config-${REGISTRY_MOCK_PORT}.yaml`);
+const configPath = () => join(__dirname, `../.temp/runtime-configs/runtime-config-${REGISTRY_MOCK_PORT}.yaml`);
 let _storage: string;
 const storage = () => {
   if (!_storage) {
