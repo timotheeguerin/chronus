d232277ec4240f83007ad5fee1806ab057b5336d
Timothee Guerin <tiguerin@microsoft.com>
2024-03-08 20:20:11 -0800

    mock registry fork

---
+++ packages/registry-mock/src/index.ts
@@ -0,0 +1,63 @@
+import { spawn, type SpawnOptions } from "child_process";
+import { randomBytes } from "crypto";
+import { readFileSync } from "fs";
+import { mkdir, readFile, writeFile } from "fs/promises";
+import { dump, load } from "js-yaml";
+import path, { join } from "path";
+import * as locations from "./locations.js";
+
+const REGISTRY_MOCK_PORT = locations.REGISTRY_MOCK_PORT;
+
+export function start(opts: SpawnOptions) {
+  const verdaccioBin = require.resolve("verdaccio/bin/verdaccio");
+  return spawn("node", [verdaccioBin, "--config", locations.configPath(), "--listen", REGISTRY_MOCK_PORT], opts);
+}
+
+export const getIntegrity = (pkgName: string, pkgVersion: string): string => {
+  return JSON.parse(readFileSync(path.join(locations.storage(), pkgName, "package.json"), "utf8")).versions[pkgVersion]
+    .dist.integrity;
+};
+
+export { REGISTRY_MOCK_PORT };
+
+async function tempdir(): Promise<string> {
+  const uid = randomBytes(8).toString("hex");
+  const path = join(process.cwd(), `.temp/tests/${uid}`);
+  await mkdir(path, { recursive: true });
+  return path;
+}
+
+export async function prepare() {
+  const storage = await tempdir();
+  const content = await readFile(path.join(__dirname, "../registry/config.yaml"));
+  const config: any = load(content.toString());
+  await writeFile(
+    locations.configPath(),
+    dump({
+      ...config,
+      storage,
+      uplinks: {
+        npmjs: {
+          url: process.env["PNPM_REGISTRY_MOCK_UPLINK"] || "https://registry.npmjs.org/",
+          // performance improvements
+          // https://verdaccio.org/docs/en/uplinks
+
+          // avoid go to uplink is offline
+          max_fails: 100,
+          // default is 10 min, avoid hit the registry for metadata
+          maxage: "30m",
+          // increase threshold to avoid uplink is offline
+          fail_timeout: "10m",
+          // increase threshold to avoid uplink is offline
+          timeout: "600s",
+          // pass down to request.js
+          agent_options: {
+            keepAlive: true,
+            maxSockets: 40,
+            maxFreeSockets: 10,
+          },
+        },
+      },
+    }),
+  );
+}
---
+++ packages/registry-mock/src/locations.ts
@@ -0,0 +1,17 @@
+import { readFileSync } from "fs";
+import { load } from "js-yaml";
+import { join } from "path";
+const REGISTRY_MOCK_PORT = process.env["CHRONUS_REGISTRY_MOCK_PORT"] || "4873";
+
+const registry = () => join(__dirname, "../registry");
+const configPath = () => join(registry(), `runtime-config-${REGISTRY_MOCK_PORT}.yaml`);
+let _storage: string;
+const storage = () => {
+  if (!_storage) {
+    const content = readFileSync(configPath());
+    _storage = (load(content.toString()) as { storage: string }).storage;
+  }
+  return _storage;
+};
+
+export { REGISTRY_MOCK_PORT, configPath, storage };
