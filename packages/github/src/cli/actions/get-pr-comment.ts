8921171301969d8e54d96f71585c212c65b8e973
Timothee Guerin <tiguerin@microsoft.com>
2024-05-05 16:41:15 -0700

    Abc

---
+++ packages/github/src/cli/actions/get-pr-comment.ts
@@ -0,0 +1,23 @@
+import { writeFile } from "fs/promises";
+import { getPullRequestContext, resolveChangeStatusCommentForPr } from "../../pull-requests/index.js";
+
+export interface GetPRCommentArgs {
+  readonly out?: string;
+}
+
+export async function getPrComment(args: GetPRCommentArgs) {
+  const context = getPullRequestContext();
+  const content = await resolveChangeStatusCommentForPr(context);
+
+  if (args.out === undefined) {
+    log(content);
+  } else {
+    log("Writing comment to file: " + args.out);
+    await writeFile(args.out, content);
+  }
+}
+
+function log(message: string) {
+  // eslint-disable-next-line no-console
+  console.log(message);
+}
